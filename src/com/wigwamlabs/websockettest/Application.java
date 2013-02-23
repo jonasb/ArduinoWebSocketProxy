88b4a7d97df24096fcf4267ca4d3eb15af66192d
Jonas Bengtsson <jonas.b@gmail.com>
2013-02-23 20:56:47 +0100

    enable strictmode

---
+++ src/com/wigwamlabs/websockettest/Application.java
@@ -0,0 +1,22 @@
+package com.wigwamlabs.websockettest;
+
+import android.os.StrictMode;
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyFlashScreen()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+    }
+}
